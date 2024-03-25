45cb17d94bb3af5f8f4eae34078cd0f630af8c44
Jonas Souza <Jonassouza305@yahoo.com.br>
2024-03-25 08:45:39 -0300

    feat: adds responsivenes and implements the hamburger menu so  `header` adapts to mobile phones

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
 import { Header } from "./components/header";
+import { Carousel } from "./components/carousel";
+import { register } from "swiper/element/bundle";
+register();
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 const Home = () => {
   return (
-    <main>
-      <Header></Header>
-    </main>
+    <>
+      <Header />
+      <Carousel />
+    </>
   );
 };
 
