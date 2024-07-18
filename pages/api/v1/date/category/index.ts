3b2b5fdff231b2bc8a9977aeff04d6c2f79493a8
Jonas Souza <Jonassouza305@yahoo.com.br>
2024-07-17 23:14:45 -0300

    desfazer mudança

--- pages/api/v1/date/category/index.ts
+++ pages/api/v1/date/category/index.ts
@@ -1,21 +1,11 @@
 import database from "@/infra/database";
-import Cors from "cors";
-import initMiddleware from "lib/init-middleware";
 
-const cors = initMiddleware(
-  Cors({
-    origin: "*",
-    methods: ["GET", "POST", "OPTIONS"],
-  }),
-);
-
-async function datesCategory(req, res) {
-  await cors(req, res);
+async function datesCategory(request, response) {
   const dates = await database.query(`SELECT * FROM category`);
 
   const result = await dates.rows;
 
-  res.status(200).json({
+  response.status(200).json({
     result,
   });
 }
