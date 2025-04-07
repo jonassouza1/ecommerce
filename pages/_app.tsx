9038240f93e311dd6e186d0782d414b5af8d5a38
jonas souza <jonassouza305@yahoo.com.br>
2025-04-06 21:12:57 -0300

    --amend

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,14 +5,13 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-    <Head>
-    <script
-      async
-      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-xxxxxxxxxxxxxxxx"
-      crossOrigin="anonymous"
-    ></script>
-  </Head>
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8813898860813456"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
     </>
-
   );
 }
