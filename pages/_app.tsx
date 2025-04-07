9b8e1b26130f08ad5d247081b71720adce5d754c
jonas souza <jonassouza305@yahoo.com.br>
2025-04-06 21:04:46 -0300

    feat: implement google adsense in `heade`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,18 @@
 import "styles/global.css";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+    <Head>
+    <script
+      async
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-xxxxxxxxxxxxxxxx"
+      crossOrigin="anonymous"
+    ></script>
+  </Head>
+    </>
+
+  );
 }
